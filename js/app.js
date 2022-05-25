23d23dd44637a7f5b02456f86a63e43daff38802
Gustavo Oliveira <dogustavo.oliveira@gmail.com>
2022-05-25 09:02:57 -0300

    feat: finalizado cache

--- js/app.js
+++ js/app.js
@@ -1,49 +1,61 @@
-const root = document.getElementById('root');
+const root = document.getElementById('root')
 
+var headers = new Headers()
+headers.append('Service-Worker-Allowed', '/app.html')
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker
       .register('/sw.js')
       .then((res) => console.log('service worker registered'))
-      .catch((err) => console.log('service worker not registered', err));
-  });
+      .catch((err) =>
+        console.log('service worker not registered', err)
+      )
+  })
 
-  window.addEventListener('load', getSpaceStationPosition);
+  window.addEventListener('load', getSpaceStationPosition)
 }
 
 async function getSpaceStationPosition() {
-  const response = await fetch('http://api.open-notify.org/iss-now.json');
-  const data = await response.json();
+  const response = await fetch(
+    'http://api.open-notify.org/iss-now.json'
+  )
+  const data = await response.json()
 
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showLocation, showError);
+    navigator.geolocation.getCurrentPosition(showLocation, showError)
 
     function showLocation(location) {
-      const source = document.getElementById('position');
+      const source = document.getElementById('position')
 
       const mapOptions = {
         center: [location.coords.latitude, location.coords.longitude],
-        zoom: 3,
-      };
+        zoom: 3
+      }
 
-      let map = L.map('map', mapOptions);
+      let map = L.map('map', mapOptions)
       let layer = new L.TileLayer(
         'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      );
+      )
 
-      L.circle([location.coords.latitude, location.coords.longitude], {
-        color: 'red',
-        fillOpacity: 0.5,
-        radius: 100000,
-      }).addTo(map);
+      L.circle(
+        [location.coords.latitude, location.coords.longitude],
+        {
+          color: 'red',
+          fillOpacity: 0.5,
+          radius: 100000
+        }
+      ).addTo(map)
 
-      L.circle([data?.iss_position.latitude, data?.iss_position.longitude], {
-        color: 'blue',
-        fillOpacity: 0.5,
-        radius: 100000,
-      }).addTo(map);
+      L.circle(
+        [data?.iss_position.latitude, data?.iss_position.longitude],
+        {
+          color: 'blue',
+          fillOpacity: 0.5,
+          radius: 100000
+        }
+      ).addTo(map)
 
-      map.addLayer(layer);
+      map.addLayer(layer)
 
       const position = distance(
         location.coords.latitude,
@@ -51,42 +63,44 @@ async function getSpaceStationPosition() {
         data?.iss_position.latitude,
         data?.iss_position.longitude,
         'K'
-      );
+      )
 
-      source.innerHTML = `Você está a ${position}Km da estação espacial ISS`;
+      source.innerHTML = `Você está a ${position}Km da estação espacial ISS`
     }
   }
 }
 
 function showError(err) {
   if (err.PERMISSION_DENIED) {
-    alert('Para funcionar é necessário conceder a permissão de localização');
+    alert(
+      'Para funcionar é necessário conceder a permissão de localização'
+    )
   }
 }
 
 function distance(lat1, lon1, lat2, lon2, unit) {
   if (lat1 == lat2 && lon1 == lon2) {
-    return 0;
+    return 0
   } else {
-    var radlat1 = (Math.PI * lat1) / 180;
-    var radlat2 = (Math.PI * lat2) / 180;
-    var theta = lon1 - lon2;
-    var radtheta = (Math.PI * theta) / 180;
+    var radlat1 = (Math.PI * lat1) / 180
+    var radlat2 = (Math.PI * lat2) / 180
+    var theta = lon1 - lon2
+    var radtheta = (Math.PI * theta) / 180
     var dist =
       Math.sin(radlat1) * Math.sin(radlat2) +
-      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta)
     if (dist > 1) {
-      dist = 1;
+      dist = 1
     }
-    dist = Math.acos(dist);
-    dist = (dist * 180) / Math.PI;
-    dist = dist * 60 * 1.1515;
+    dist = Math.acos(dist)
+    dist = (dist * 180) / Math.PI
+    dist = dist * 60 * 1.1515
     if (unit == 'K') {
-      dist = dist * 1.609344;
+      dist = dist * 1.609344
     }
     if (unit == 'N') {
-      dist = dist * 0.8684;
+      dist = dist * 0.8684
     }
-    return dist.toFixed(2);
+    return dist.toFixed(2)
   }
 }
--- sw.js
+++ sw.js
@@ -1,46 +1,34 @@
-const CACHE_NAME = 'findme-v8';
+const CACHE_NAME = 'findme-v8'
 const assets = [
   '/',
   '/app.html',
   'js/instalar.js',
   '/js/app.js',
   '/manifest.json',
+  '/img/icon.png',
   'https://unpkg.com/leaflet@1.8.0/dist/leaflet.css',
   'https://unpkg.com/leaflet@1.8.0/dist/leaflet.js',
-  'http://api.open-notify.org/iss-now.json',
-  '3.png',
-  '4.png',
-  '5.png',
-];
+  'http://api.open-notify.org/iss-now.json'
+]
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[Service Worker] Caching all: app shell and content');
-      return cache.addAll(assets);
+      console.log(
+        '[Service Worker] Caching all: app shell and content'
+      )
+      return cache.addAll(assets)
     })
-  );
-});
+  )
+})
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    (async () => {
-      const res = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-
-      if (res) return res;
-
-      const newRes = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-
-      cache.open(e.request, newRes.clone());
-
-      return newRes;
-    })()
-  );
-});
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request)
+    })
+  )
+})
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
@@ -48,10 +36,10 @@ self.addEventListener('activate', (e) => {
       return Promise.all(
         keyList.map((key) => {
           if (key !== CACHE_NAME) {
-            return caches.delete(key);
+            return caches.delete(key)
           }
         })
-      );
+      )
     })
-  );
-});
+  )
+})
